c9627b85333a3ac33a10e07791cb5ec4c9f4237a
Nadiia Katyreva <82587359+Katireva@users.noreply.github.com>
2024-06-13 19:47:07 +0100

    Revert "first profile commit"

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "nadiia-food-ordering.s3.amazonaws.com",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -1,7 +1,6 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react"; // Import Provider as SessionProvider
-
+import { SessionProvider } from "next-auth/react";
 export function AppProvider({ children }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
